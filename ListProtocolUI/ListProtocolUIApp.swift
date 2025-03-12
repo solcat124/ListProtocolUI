59abce956783b44f7d3dde1b5613710ae543d3a0
solcat124 <pk80@windbow.com>
2025-03-12 07:46:09 -0700

    Initial Commit

---
+++ ListProtocolUI/ListProtocolUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListProtocolUIApp.swift
+//  ListProtocolUI
+//
+//  Created by Phil Kelly on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ListProtocolUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListProtocolUITests/ListProtocolUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ListProtocolUITests.swift
+//  ListProtocolUITests
+//
+//  Created by Phil Kelly on 3/12/25.
+//
+
+import Testing
+@testable import ListProtocolUI
+
+struct ListProtocolUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
